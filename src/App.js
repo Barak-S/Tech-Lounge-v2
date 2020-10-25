a77ffa64f413f6afcaaa947c9b7fd0fc9cc20310
Barak-S <barak.saidoff@gmail.com>
2020-10-25 14:35:40 -0400

    background circle svg and styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar'
 import { Col, Row, Card } from 'react-bootstrap';
+import Beclipse from './images/big-eclipse.svg'
+import Meclipse from './images/mid-eclipse.svg'
+import Seclipse from './images/small-eclipse.svg'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>	
       <div className="container">
-            <img style={{ width: "100%", position: "relative", objectFit: "cover"}} src="https://images.unsplash.com/photo-1517059224940-d4af9eec41b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2067&q=80" />
+            {/* <img style={{ width: "100%", position: "relative", objectFit: "cover"}} src={"https://images.unsplash.com/photo-1517059224940-d4af9eec41b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2067&q=80"} alt="cannot display" /> */}
             <div style={{position: "absolute", top: 165, left: 0, width: "100%" }}>
               <h1>Update your work atmosphere.</h1>
               <p style={{fontSize: 20, margin: 15}}>Improve your productivity.</p>
@@ -18,9 +21,9 @@ function App() {
               </div>
             </div>
       </div>		
-			<img class="big-circle" src={require("./images/big-eclipse.svg")} alt="" />
-			<img class="medium-circle" src={require("./images/mid-eclipse.svg")} alt="" />
-			<img class="small-circle" src={require("./images/small-eclipse.svg")} alt="" />
+			<img class="big-circle" src={Beclipse} alt="" />
+			<img class="medium-circle" src={Meclipse} alt="" />
+			<img class="small-circle" src={Seclipse} alt="" />
     </div>
   );
 }
