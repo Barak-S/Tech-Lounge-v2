f73412e8970f9f6ca9c08155558df89bb1565a95
Barak-S <barak.saidoff@gmail.com>
2020-10-25 14:22:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './NavBar'
+import { Col, Row, Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>	
+      <div className="container">
+            <img style={{ width: "100%", position: "relative", objectFit: "cover"}} src="https://images.unsplash.com/photo-1517059224940-d4af9eec41b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2067&q=80" />
+            <div style={{position: "absolute", top: 165, left: 0, width: "100%" }}>
+              <h1>Update your work atmosphere.</h1>
+              <p style={{fontSize: 20, margin: 15}}>Improve your productivity.</p>
+              <div class="cta">
+                <button class="cta-add">View Collection</button>
+              </div>
+            </div>
+      </div>		
+			<img class="big-circle" src={require("./images/big-eclipse.svg")} alt="" />
+			<img class="medium-circle" src={require("./images/mid-eclipse.svg")} alt="" />
+			<img class="small-circle" src={require("./images/small-eclipse.svg")} alt="" />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+// import { Link, withRouter } from "react-router-dom";
+
+class NavBar extends React.Component {
+
+    render(){
+
+        return (
+            <Navbar collapseOnSelect expand="lg" style={{  backdropFilter: "blur(6px)"}}>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="justify-content-center" style={{ flex: 1}}>
+                        <Nav.Item>
+                            <Nav.Link style={{  color: "rgb(100,124,187)"}} href="/home">Furniture</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link style={{  color: "rgb(100,124,187)"}} href="/home">Accessories</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link style={{  color: "rgb(100,124,187)"}} href="/home">Contact</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link style={{  color: "rgb(100,124,187)"}} href="/home">Blog</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+        );
+
+
+    }
+}
+
+export default NavBar;
