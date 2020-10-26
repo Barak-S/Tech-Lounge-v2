4e35d9e97390eebc8c763cc69ac4ab68898c74a7
Barak-S <barak.saidoff@gmail.com>
2020-10-26 11:45:26 -0400

    install gsap, and added animation delays to header1, header2, and button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,83 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar'
-import { Col, Row, Card } from 'react-bootstrap';
+// import { Col, Row, Card } from 'react-bootstrap';
 import Beclipse from './images/big-eclipse.svg'
 import Meclipse from './images/mid-eclipse.svg'
 import Seclipse from './images/small-eclipse.svg'
 
+import { TweenMax, Power3 }  from 'gsap'
+
 function App() {
+
+  let heading = useRef(null)
+  let heading2 = useRef(null)
+  let buttonCollection = useRef(null)
+
+  useEffect(()=>{
+    console.log(heading, heading2)
+    TweenMax.to(
+      heading,
+      .8,
+      {
+        opacity: 1,
+        y: 10,
+        ease: Power3.easeOut
+      }
+    )
+    TweenMax.to(
+      heading2,
+      .8,
+      {
+        opacity: 1,
+        y: 15,
+        ease: Power3.easeOut,
+        delay: .8
+      }
+    )
+    TweenMax.to(
+      buttonCollection,
+      .8,
+      {
+        opacity: 1,
+        y: -15,
+        ease: Power3.easeOut,
+        delay: 1.6
+      }
+    )
+  }, [])
+
+  // console.log(heading)
   return (
     <div className="App">
       <NavBar/>	
       <div className="container">
-            {/* <img style={{ width: "100%", position: "relative", objectFit: "cover"}} src={"https://images.unsplash.com/photo-1517059224940-d4af9eec41b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2067&q=80"} alt="cannot display" /> */}
-            <div style={{position: "absolute", top: 165, left: 0, width: "100%" }}>
-              <h1>Update your work atmosphere.</h1>
-              <p style={{fontSize: 20, margin: 15}}>Improve your productivity.</p>
+            <div style={{position: "absolute", top: 200, left: 0, width: "100%" }}>
+              <h1 
+                className="firstHeader" 
+                ref={el => {heading = el}}
+              >Update your work atmosphere.
+              </h1>
+              <p 
+                className="secondHeader" 
+                ref={el => {heading2 = el}} 
+                style={{fontSize: 20, margin: 15}}
+              >Improve your productivity.
+              </p>
               <div class="cta">
-                <button class="cta-add">View Collection</button>
+                <button 
+                  ref={el => {buttonCollection = el}} 
+                  class="cta-add"
+                >View Collection
+                </button>
               </div>
             </div>
       </div>		
 			<img class="big-circle" src={Beclipse} alt="" />
 			<img class="medium-circle" src={Meclipse} alt="" />
 			<img class="small-circle" src={Seclipse} alt="" />
+      
     </div>
   );
 }
