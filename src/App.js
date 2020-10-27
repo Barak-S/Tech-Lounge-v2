ddf642e85ba978443dd3b90ecb905982906d0a3f
Barak-S <barak.saidoff@gmail.com>
2020-10-27 09:58:50 -0400

    added nyc jpeg bg and added onClick with gsap to change bg url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useRef, useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { gsap } from "gsap";
 import "./App.scss";
-import { TweenMax, TweenLite, Power3 }  from 'gsap';
-import Header from "./Header";
 import Home from './Home'
 import NavBar from './NavBar'
-
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import React, { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
 import "./App.scss";
 import { TweenMax, TweenLite, Power3 }  from 'gsap';
-import Header from "./Header";
+import { Row, Col } from 'react-bootstrap';
 
-import newyork from "./images/newyork.webp";
+import newyork from "./images/nyc.jpeg";
 import telaviv from "./images/telaviv.jpeg"
 
 
@@ -15,34 +15,55 @@ const cities = [
   
 const Home = () => {
 
-    // let img = useRef(null);
-    // useEffect(()=>{
-    //   console.log(img)
-    //   TweenMax.fromTo(
-    //     img,
-    //     1.2,
-    //     { opacity: 0, y: 50 },
-    //     { opacity: 1, y: 0 }
-    //   )
-    // }, [])
+    let home = useRef(null);
+    let app = useRef(null)
+
+    useEffect(()=>{
+      TweenMax.to( home, 0, {css: {visibility: 'visible'}})
+      TweenMax.fromTo( home, 1.2,{ opacity: 0, y: 50 },{ opacity: 1, y: 0 })
+    }, [])
+
+    const handleCity = (city) => {
+        console.log(city)
+        TweenMax.to(home, {
+            duration: 0,
+            background: `url(${city}) center center`
+        });
+        TweenMax.to(home, {
+            duration: 0.4,
+            opacity: 1,
+            ease: "power3.inOut"
+        });
+        TweenMax.from(home, {
+            duration: 0.4,
+            skewY: 2,
+            transformOrigin: "right top"
+        });
+        
+    }
   
     return (
-      <div className="home">
-        {/* <img ref={el => (img = el)} src={telaviv}/> */}
+      <div ref={el => (home = el)}className="home">
         <div style={{position: "absolute", top: "50%"}}>
-            <h3>Our Mission</h3>
-            <p>
-            With attention to detail, we continue to shape the future of the tech industry.
-            </p>
-            <div className='locations'>
-            Locations:
-            {cities.map(el => (
-                <span
-                key={el.name}>
-                {el.name}
-                </span>
-            ))}
-        </div>
+            <Row>
+                <Col xs={12} sm={12} md={8} lg={8} className="align-auto">
+                    <h2 style={{ color: "rgb(100,124,187)"}}>Our Mission</h2>
+                    <p style={{color: "#ffffff"}}>
+                    With attention to detail, we continue to shape the future of the tech industry.
+                    </p>
+                    <div className='locations'>
+                    Locations:
+                    {cities.map(el => (
+                        <span
+                        key={el.name}
+                        onClick={()=>handleCity(el.image)}
+                        >
+                        {el.name}
+                        </span>
+                    ))}
+                    </div>
+                </Col>
+            </Row>
 
         </div>
       </div>
