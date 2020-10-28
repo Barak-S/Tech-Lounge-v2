62e506cef6658acd567fcc48aad8841b0a5dde2e
Barak-S <barak.saidoff@gmail.com>
2020-10-28 12:15:55 -0400

    shopping cart staged template

--- src/Cart.js
+++ src/Cart.js
@@ -6,36 +6,31 @@ class Cart extends Component {
         return (
             <div className="cart">
                 <div style={{paddingTop: "10vh"}}>
-                    <Col xs={12} sm={12} md={10} lg={10} className="align-auto">
-                        <h2>Shopping Cart</h2>
-                        <Table striped bordered hover responsive>
-                            <thead style={{fontSize: 11}}>
-                                <tr>
-                                <th>2 item's in your cart</th>
-                                <th>Products</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td>Nook Pod</td>
-                                <td></td>
-                                </tr>
-                                <tr>
-                                <td>"Hello World" Sign</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                                </tr>
-                                <tr>
-                                <td>3</td>
-                                <td colSpan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Col>
+                        <Col xs={12} sm={12} md={10} lg={10} className="align-auto">
+                            <h2>Shopping Cart</h2>
+                            <Table striped bordered hover>
+                                <thead style={{fontSize: 11}}>
+                                    <tr>
+                                    <th>Products</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Nook Pod</td>
+                                        <td>1</td>
+                                        <td>$9,500.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>"Hello World" sign"</td>
+                                        <td>2</td>
+                                        <td>$34.99</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+
                 </div>
             </div>
         );
